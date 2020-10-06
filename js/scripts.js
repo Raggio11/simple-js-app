af26c9f555dc0d5a1364cd814b874392ae9f05f6
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-06 14:14:25 -0600

    changed from object to array
    Formating was still not correct, adjusting to make array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-let pokemonList = {
-  Pokemon1 : { name:"Bulbasaur", height: .7, type: ["grass","posion"]},
-  Pokemon2 : { name:"Ivysour", height: 1, type: ["grass","posion"]},
-  Pokemon3 : { name:"Venusaur", height: 2, type: ["grass","posion"]},
-};
+let pokemonList = [
+  { name: 'Bulbasaur', height: .7, type: ['grass','posion']},
+  { name: 'Ivysour', height: 1, type:  ['grass','posion']},
+  { name: 'Venusaur', height: 2, type:  ['grass','posion']},
+];
 console.log(pokemonList);
