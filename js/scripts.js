3586f9bb81f12de5e50b1653e4b3140e225b767d
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-05 20:23:05 -0600

    Creating Pokemon list
    Started creating the list for the Pokedex, wrote down the first three pokemon on the list and creating a PokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-alert('Hello world');
-
-let favoriteFood = 'Cafe rio'
-document.write(favoriteFood)
+let pokemonList = [];
+let arrayofPokemon : [{ Name:Bulbasaur, height: .7, types: ["grass" and "posion"]}]
+let arrayofPokemon : [{Name:Ivysour, height: 1, types: ["grass" and "posion"]}]
+let arrayofPokemon : [{Name:Venusaur, height: 2, types: ["grass" and "posion"]}]
