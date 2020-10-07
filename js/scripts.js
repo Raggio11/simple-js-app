f27bd6962c7d755efec856847ae6be02aec98b3e
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-07 10:13:01 -0600

    Change from equal to ranged heights
    Changing the variables to look for a range of min and max height  instead of the exact equal height.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ let pokemonList = [
   { name: 'Venusaur', height: 2, type:  ['grass','posion']}
 ];
 for (let i=0; i< pokemonList.length; i++){
-  if (pokemonList[i].height === .7 ){
+  if (pokemonList[i].height <.8 && pokemonList[i].height >.6){
     document.write(pokemonList[i].name + "(height:.7)")
   }
-  else if (pokemonList[i].height === 1 ){
+  else if (pokemonList[i].height <1.5 && pokemonList[i].height >.9){
     document.write(pokemonList[i].name + "(height:1)")
   }
-  else if (pokemonList[i].height === 2){
+  else if (pokemonList[i].height <2.5 && pokemonList[i].height >1.9){
     document.write(pokemonList[i].name + "(height:2)" + "- Wow, that's big!")
   }
 }
