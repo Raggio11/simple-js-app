5bba188716426475ecadf75038272fdfd3831648
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-07 19:34:23 -0600

    Update structure of Loop
    Use a better loop to create a better system for the pokedex, added a pokedex number to all of the pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 let pokemonList = [
-  { name: 'Bulbasaur', height: .7, type: ['grass','posion']},
-  { name: 'Ivysour', height: 1, type:  ['grass','posion']},
-  { name: 'Venusaur', height: 2, type:  ['grass','posion']}
-];
-for (let i=0; i< pokemonList.length; i++){
-  if (pokemonList[i].height <.8 && pokemonList[i].height >.6){
-    document.write(pokemonList[i].name + "(height:.7)")
-  }
-  else if (pokemonList[i].height <1.5 && pokemonList[i].height >.9){
-    document.write(pokemonList[i].name + "(height:1)")
-  }
-  else if (pokemonList[i].height <2.5 && pokemonList[i].height >1.9){
-    document.write(pokemonList[i].name + "(height:2)" + "- Wow, that's big!")
-  }
-}
+  {name: 'Bulbasaur',
+  pokedexNumber: 1,
+  height: .7,
+  type: ['grass','posion']
+  },
+  { name: 'Ivysour',
+  pokedexNumber: 2,
+  height: 1,
+  type:  ['grass','posion']
+  },
+  { name: 'Venusaur',
+  pokedexNumber: 3,
+  height: 2,
+  type:  ['grass','posion']}
+  ];
+  for (let i = 0; i < pokemonList.length; i++) {
+      document.write("#" + pokemonList[i].pokedexNumber + " " + pokemonList[i].name + " ");
+      if (pokemonList[i].height >= 1.5) {
+          document.write("(height: " + pokemonList[i].height + ") - Wow that's big!</br>")
+      }
+      else if (pokemonList[i].height < 1.5)
+          document.write("(height: " + pokemonList[i].height + ")</br>")
+  };
