1226869ba9fe17a97f517e4aab513b45b92778ad
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-12 14:26:53 -0600

    Wrapping in a IlFE
    Wrapped all the code into a IIFE, program runs the same.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,39 @@
-let pokemonList = [
-  {name: 'Bulbasaur',
-  pokedexNumber: 1,
-  height: .7,
-  type: ['grass','posion']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur',
+    pokedexNumber: 1,
+    height: .7,
+    type: ['grass','posion']
   },
   { name: 'Ivysour',
   pokedexNumber: 2,
   height: 1,
   type:  ['grass','posion']
-  },
-  { name: 'Venusaur',
-  pokedexNumber: 3,
-  height: 2,
-  type:  ['grass','posion']
-  }
-  ];
+},
+{ name: 'Venusaur',
+pokedexNumber: 3,
+height: 2,
+type:  ['grass','posion']
+}];
 
-  pokemonList.forEach(function(user){
-    document.write('#' + user.pokedexNumber + ' ' + user.name)
-;
-if (user.height >= 1.5) {
-  document.write("(height: " + user.height + ") - Wow that's big!</br>")
+function add(pokemon) {
+  pokemonList.push(pokemon);
 }
-else if (user.height < 1.5)
+function getAll() {
+  return pokemonList;
+}
+return {
+  add: add,
+  getAll: getAll
+};
+})();
+
+pokemonRepository.getAll().forEach(function(user){
+  document.write('#' + user.pokedexNumber + ' ' + user.name)
+  ;
+  if (user.height >= 1.5) {
+    document.write("(height: " + user.height + ") - Wow that's big!</br>")
+  }
+  else if (user.height < 1.5)
   document.write("(height: " + user.height + ")</br>")
 });
