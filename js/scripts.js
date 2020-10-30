af71532eaa591e144cc8e44c59fae68e78901959
Raggio11 <Raggio11@users.noreply.github.com>
2020-10-30 00:52:02 -0600

    Styling
    Adding code to improve the aesthetic  of the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,117 @@
-let pokemonRepository = (function () {
+let pokemonRepository = (function() {
   let modalContainer = document.querySelector('#modal-container');
   let container = document.querySelector('#image-container');
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  let detailsUrl = 'https://pokeapi.co/api/v2/pokemon/'
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  let detailsUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-function getAll() {
-  return pokemonList;
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-function addListItem(pokemon){
-  let pokemonList = document.querySelector("#pokemon-list"); /*connects to class pokemon-list on index.html*/
-  let listItem = document.createElement('li'); /*creats list for every pokemon on the pokemonList*/
-  let button = document.createElement("button"); /*creats a button for every item on the list*/
-  button.innerText = pokemon.name; /*adds the name of the pokemon to every button that has been created*/
-  button.classList.add('button-list');
-  button.classList.add("button-class");  /*creats a button class to connect to styles.css to change the style of the page*/
-  button.classList.add('btn');
-  button.addEventListener("click", function (event) {
-    showDetails(pokemon);}) /*creats fucntion that if clicked on will do what the showDetails function demands*/
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(
+      '#pokemon-list'
+    ); /*connects to class pokemon-list on index.html*/
+    let listItem = document.createElement(
+      'li'
+    ); /*creats list for every pokemon on the pokemonList*/
+    let button = document.createElement(
+      'button'
+    ); /*creats a button for every item on the list*/
+    button.innerText =
+      pokemon.name; /*adds the name of the pokemon to every button that has been created*/
+    button.classList.add('button-list');
+    button.classList.add(
+      'button-class'
+    ); /*creats a button class to connect to styles.css to change the style of the page*/
+    button.classList.add('btn');
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    }); /*creats fucntion that if clicked on will do what the showDetails function demands*/
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
   }
 
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
       });
-    }).catch(function (e) {
-      console.error(e);
-    })
   }
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Now we add the details to the item
-      item.name = item.name;
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-      item.weight = details.weight;
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        // Now we add the details to the item
+        item.name = item.name;
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+        item.weight = details.weight;
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
+    pokemonRepository.loadDetails(item).then(function() {
       showModal(item);
     });
   }
   function showModal(item) {
+    let modalBody = $('.modal-body');
+    let modalHeader = $('.modal-header');
+    let modalTitle = $('.modal-title');
+    let btnClose = $('#btnClose');
 
-let modalBody = $('.modal-body');
-let modalHeader = $('.modal-header');
-let modalTitle = $('.modal-title');
-let btnClose = $('#btnClose');
+    // Implementing modal Container
+    $('#modal-container').modal('show');
 
-// Implementing modal Container
-$('#modal-container').modal('show');
+    modalHeader.empty();
+    modalTitle.empty();
+    modalBody.empty();
 
-modalHeader.empty();
-modalTitle.empty();
-modalBody.empty();
+    //creating element for name in modal textContent
+    let nameElement = $('<h3>' + item.name + '</h3>');
 
-//creating element for name in modal textContent
-let nameElement = $('<h3>' + item.name + '</h3>');
+    //creating img modal textContent
+    let imageElement = $('<img class="modal-img">');
+    imageElement.attr('src', item.imageUrl);
 
-//creating img modal textContent
-let imageElement = $('<img class="modal-img">');
-imageElement.attr('src', item.imageUrl);
+    //creating element for height in modal textContent
+    let heightElement = $('<p>' + 'Height : ' + item.height + '</p>');
 
-//creating element for height in modal textContent
-let heightElement = $('<p>' + 'Height : ' + item.height + '</p>');
+    //creating element for weight in modal textContent
+    let weightElement = $('<p>' + 'Weight : ' + item.weight + '</p>');
 
-//creating element for weight in modal textContent
-let weightElement = $('<p>' + 'Weight : ' + item.weight + '</p>');
-
-
-modalHeader.append(nameElement);
-modalBody.append(imageElement);
-modalBody.append(heightElement);
-modalBody.append(weightElement);
-modalHeader.append(btnClose);
-}
-document.querySelector('button').addEventListener('click', () => {
-  showModal();
-});
+    modalHeader.append(nameElement);
+    modalBody.append(imageElement);
+    modalBody.append(heightElement);
+    modalBody.append(weightElement);
+    modalHeader.append(btnClose);
+  }
+  document.querySelector('button').addEventListener('click', () => {
+    showModal();
+  });
 
   return {
     add: add,
@@ -105,13 +119,12 @@ document.querySelector('button').addEventListener('click', () => {
     addListItem: addListItem,
     showDetails: showDetails,
     loadList: loadList,
-    loadDetails: loadDetails,
+    loadDetails: loadDetails
   };
 })();
 
-
-pokemonRepository.loadList().then(function () {
-  pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
